a7625ac51242aad60469020f6db2478c0bc5d7f9
rgbalex <alex.m.angelidis@gmail.com>
2024-05-24 18:47:38 +0100

    Node implementation for terminal and iterable support

--- itb/minimax/nodes.py
+++ itb/minimax/nodes.py
@@ -1,8 +1,19 @@
 class Node:
+    parent = None  # type: Node
+    children = []  # type: list[Node]
+    state = None  # type: tuple[tuple[int,int,int,int]]
+
+    def __init__(self, state, parent=None):
+        self.state = state
+        self.parent = parent
+
     def heuristic_value(self):
         # TODO: Implement heuristic value
         pass
 
+    def is_terminal(self):
+        return len(self.children) == 0
+
     def __iter__(self):
-        # TODO: Implement iterator
-        return []
+        for child in self.children:
+            yield child
