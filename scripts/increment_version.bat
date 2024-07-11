f45b7c7e6dff93d066adc4f08b6263dd0ef226b0
Alexander Angelidis <alex.m.angelidis@gmail.com>
2024-07-12 00:31:05 +0100

    edited script to support alpha and beta tagging

--- scripts/increment_version.bat
+++ scripts/increment_version.bat
@@ -1,11 +1,12 @@
 @echo off
 set /p VERSION=<.env
-for /f "tokens=1,2,3 delims=." %%a in ("%VERSION%") do (
+for /f "tokens=1,2,3,4 delims=.-" %%a in ("%VERSION%") do (
     set "MAJOR=%%a"
     set "MINOR=%%b"
     set "PATCH=%%c"
+    set "BUILD=%%d"
 )
 set /a "PATCH+=1"
-set "NEW_VERSION=%MAJOR%.%MINOR%.%PATCH%"
+set "NEW_VERSION=%MAJOR%.%MINOR%.%PATCH%-%BUILD%"
 >.env echo %NEW_VERSION%
 git add .env
